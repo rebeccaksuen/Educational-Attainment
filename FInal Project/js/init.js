c0b55cd7a3d10a24a0f96cd660a9ff583bb82a63
em14 <ELSAMAI14@G.UCLA.EDU>
2023-06-07 19:25:24 -0700

    Co-authored-by: rebeccaksuen <rebeccaksuen@users.noreply.github.com> Co-authored-by: Stephanie Chung <trufflelover@users.noreply.github.com>

--- FInal Project/js/init.js
+++ FInal Project/js/init.js
@@ -1,7 +1,6 @@
-// declare variables
+
 let mapOptions = {'center': [34.0709,-118.444],'zoom':6}
 
-// use the variables
 const map = L.map('the_map').setView(mapOptions.center, mapOptions.zoom);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -60,24 +59,3 @@ function processData(results){
 loadData(dataUrl)
 
 document.getElementById("defaultOpen").click();
-
-function openCity(evt, cityName) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
-  
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-  
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-  
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
-}
