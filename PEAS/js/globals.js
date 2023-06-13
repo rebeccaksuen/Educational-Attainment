71ff6e6e951cb49c9dc30bdc54af8b49cad87234
em14 <ELSAMAI14@G.UCLA.EDU>
2023-06-12 18:42:48 -0700

    Co-authored-by: albertkun

---
+++ PEAS/js/globals.js
@@ -0,0 +1,32 @@
+// declare variables
+let mapOptions = {'center': [34.0709,-118.444],'zoom':7}
+
+// use the variables
+const map = L.map('the_map').setView(mapOptions.center, mapOptions.zoom);
+
+// more variables
+let supportReceived = 0; //chart variables
+let supportNotReceived = 0; 
+let supportUnsure = 0;
+
+let theChart;
+let yesSupport = L.featureGroup(); // layer and map toggle variables
+let noSupport = L.featureGroup();
+let unsureSupport = L.featureGroup();
+
+let layers = {
+    "Received Family Support": yesSupport,
+    "Did Not Receive Family Support": noSupport,
+    "Unsure": unsureSupport
+}
+
+let circleOptions = {
+    radius: 5,
+    fillColor: "#ff7800",
+    color: "#",
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 0.7
+}
+
+const dataUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQSn415MNwpNpFmQyxj2WbVnRJSDx85ki66G7zrcfeHpl8DSiErm9xD8psQxTwbPAzDLQeRMI8kF6eR/pub?output=csv"
